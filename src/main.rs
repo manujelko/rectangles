31dcbaf6d65e10449e65ae4fcc81fe75d527975f
Manuel Šarić <manujelko@gmail.com>
2023-03-07 16:15:55 +0100

    Associated functions

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    // fn area(&self) -> u32 {
-        // self.width * self.height
-    // }
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
 
@@ -30,4 +37,7 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(25);
+    dbg!(square);
 }
